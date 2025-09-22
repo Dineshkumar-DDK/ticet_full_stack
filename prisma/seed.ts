3a9eb5a335276bc7f8bddc25916f3fc5d3da960a
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-09-22 07:53:48 +0530

    * function written for seeding * in seed function enum based typing made for status to solve seeding not done problem. * used createMany({}) and deleteMany({}) ORM methods from prisma * removed manual id in the mock data and let prisma handle the default id

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient,TicketStatus } from "@prisma/client"
+const prisma = new PrismaClient();
+const tickets = [
+    {
+        title: "ticket 1",
+        content: "this is the first ticket from db",
+        status: TicketStatus.OPEN,
+    },
+    {
+        title: "ticket 2",
+        content: " this is the second ticket from db",
+        status: TicketStatus.IN_PROGRESS
+    },
+    {
+        title: "ticket 3",
+        content: "this is the third ticket from db",
+        status: TicketStatus.CLOSED
+    }
+]
+
+const seed = async () => {
+    console.log("Seeding started...")
+    const t0 = performance.now();
+    try {
+        await prisma.ticket.deleteMany({});
+
+        await prisma.ticket.createMany(
+            { data: tickets }
+        )
+    }
+    catch(err){
+        console.log(`error with prisma - ${err}`)
+    }
+   
+    const t1 = performance.now()
+    console.log(`Seeding done within ${t1 - t0} seconds`)
+}
+
+seed();
