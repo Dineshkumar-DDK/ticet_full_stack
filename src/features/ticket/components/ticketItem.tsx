9827874fa26ca4abd182531cecc22b4632ca05d8
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-09-23 08:54:32 +0530

    converted ticket item to client component for using event handlers

--- src/features/ticket/components/ticketItem.tsx
+++ src/features/ticket/components/ticketItem.tsx
@@ -1,9 +1,10 @@
+'use client'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import * as Paths from "@/app/paths";
 import { TICKET_ICONS } from "../constants";
 import { Button } from "@/components/ui/button";
-import { LucideSquareArrowOutUpRight } from "lucide-react";
+import { LucideSquareArrowOutUpRight, LucideTrash } from "lucide-react";
 import clsx from "clsx";
 import { Ticket } from "@prisma/client";
 
@@ -16,12 +17,22 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
     const detailButton = (
         <Button variant="outline" size="icon" asChild>
             <Link href={Paths.ticketPath(ticket.id)} className='underline'>
-                <LucideSquareArrowOutUpRight />
+                <LucideSquareArrowOutUpRight className="h-4 w-4" />
             </Link>
         </Button>
     )
+
+    function handleDelete(){
+        console.log("hi..")
+    }
+    const deleteButton = (
+        <Button variant="outline" size="icon" onClick={handleDelete}>
+            <LucideTrash className="h-4 w-4" />
+        </Button>
+    )
+
     return (
-        <div className={clsx('flex space-x-1 w-full',{ 'max-w-[420px]':!isDetail , 'max-w-[580]':!isDetail})}>
+        <div className={clsx('flex space-x-1 w-full', { 'max-w-[420px]': !isDetail, 'max-w-[580]': !isDetail })}>
             <Card className='w-full'>
                 <CardHeader>
                     <CardTitle className="flex gap-x-2">
@@ -30,7 +41,7 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
                     </CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <span className={clsx("whitespace-break-spaces ",{'line-clamp-3':!isDetail})}>
+                    <span className={clsx("whitespace-break-spaces ", { 'line-clamp-3': !isDetail })}>
                         {ticket.content}
                     </span>
                 </CardContent>
@@ -38,11 +49,9 @@ const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
 
                 </CardFooter>
             </Card>
-            { !isDetail && 
-                <div className="flex flex-col space-y-1">
-                    {detailButton}
-                </div>
-            }
+            <div className="flex flex-col space-y-1">
+                {isDetail ? deleteButton : detailButton}
+            </div>
 
         </div>
     )
