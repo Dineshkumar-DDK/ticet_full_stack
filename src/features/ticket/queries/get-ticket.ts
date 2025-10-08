eb45e01ccadf32bd0380ab7028a0409297a1782f
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-08 20:33:21 +0530

    added cache for getTicket for request memoization

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,9 +1,10 @@
 import {prisma} from '@/lib/prisma'
+import {cache} from 'react'
 
-export const getTicket = async(id:string)=>{
+export const getTicket = cache(async(id:string)=>{
     return await prisma.ticket.findUnique({
         where:{
             id,
         }
     })
-}
+})
