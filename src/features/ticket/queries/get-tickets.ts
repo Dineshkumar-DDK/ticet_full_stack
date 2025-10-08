32efb14053ee36e12371716f8f0a3730d4015bee
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-08 10:01:59 +0530

    removed throw new error

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,11 +1,10 @@
 import {prisma} from '@/lib/prisma'
 export const getTickets=async()=>{
-    throw new Error("Database connection error")
-    // return await prisma.ticket.findMany(
-    //     {
-    //         orderBy:{
-    //             createAt:"desc",
-    //         },
-    //     }
-    // );
+    return await prisma.ticket.findMany(
+        {
+            orderBy:{
+                createAt:"desc",
+            },
+        }
+    );
 }
