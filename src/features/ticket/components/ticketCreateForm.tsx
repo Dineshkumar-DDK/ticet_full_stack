804d6342c9259e83405d4f35bc974fab196d7fce
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-09 07:33:31 +0530

    clearing errors and making the handler to run as server actions in the file. * proof is that when we click create it will show a Post request is done

--- src/features/ticket/components/ticketCreateForm.tsx
+++ src/features/ticket/components/ticketCreateForm.tsx
@@ -5,8 +5,12 @@ import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const TicketCreateForm = () => {
+  const createTicket = async(formData:FormData)=>{
+    'use server'
+    console.log(formData.get('title'))
+  }
   return (
-    <form className='flex flex-col space-y-2'>
+    <form action={createTicket} className='flex flex-col space-y-2'>
         <Label htmlFor='title'>Title</Label>
         <Input type='text' id='title' name='title' />
         <Label htmlFor='content'>Content</Label>
