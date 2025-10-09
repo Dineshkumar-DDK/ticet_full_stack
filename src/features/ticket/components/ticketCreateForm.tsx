d4c2d89c4ea7b8d83c92992bd5f9b70d2211e194
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-09 07:10:15 +0530

    * removed html default elements * installed input,textarea,label and button from shacn and used

--- src/features/ticket/components/ticketCreateForm.tsx
+++ src/features/ticket/components/ticketCreateForm.tsx
@@ -1,13 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 const TicketCreateForm = () => {
   return (
     <form className='flex flex-col space-y-2'>
-        <label htmlFor='title'>Title</label>
-        <input type='text' id='title' name='title' />
-        <label htmlFor='content'>Content</label>
-        <textarea id='content' name='content'/>
-        <button type='submit'>Create Ticket</button>    
+        <Label htmlFor='title'>Title</Label>
+        <Input type='text' id='title' name='title' />
+        <Label htmlFor='content'>Content</Label>
+        <Textarea id='content' name='content'/>
+        <Button type='submit'>Create Ticket</Button>    
     </form>
   )
 }
