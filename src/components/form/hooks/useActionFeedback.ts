b143ea7d0fe88b1d8423c385acece46f0db085b8
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-24 07:58:22 +0530

    * added type for the actions * we are giving object because we may have more than one arg so

--- src/components/form/hooks/useActionFeedback.ts
+++ src/components/form/hooks/useActionFeedback.ts
@@ -2,17 +2,17 @@ import { useEffect } from "react"
 import { ActionState } from "../utils/toActionState"
 
 type useActionFeedBackOptions={
- onSuccess?:()=>void;
- onFailure?:()=>void;
+ onSuccess?:({actionState}:{actionState:ActionState})=>void;
+ onFailure?:({actionState}:{actionState:ActionState})=>void;
 }
 
 export const useActionFeedback=(actionState:ActionState,options:useActionFeedBackOptions)=>{
     useEffect(()=>{
         if(actionState.status=="SUCCESS") {
-            options?.onSuccess?.();
+            options?.onSuccess?.({actionState});
         }
         if(actionState?.status=='ERROR'){
-            options?.onFailure?.();
+            options?.onFailure?.({actionState});
         }
     },[actionState,options])
 }
