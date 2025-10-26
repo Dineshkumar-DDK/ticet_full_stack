3fe4c33f81a94ca5f45071ff36a6683d0bed372f
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-10-26 18:56:15 +0530

    added cookie functionality to hold toast state to show toast while updating and deleting

---
+++ src/action/cookieUtils.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { cookies } from "next/headers"
+
+export const setCookieByKey = async (key: string, value: string) => {
+  const cookieStore =await cookies();
+  cookieStore?.set(key, value)
+}
+
+export const getCookieByKey = async (key:string)=>{
+  const cookieStore =await cookies();
+  const data = cookieStore?.get(key);
+  if(!data) return null;
+  return data?.value;
+  
+}
+
+export const deleteCookieByKey = async (key:string)=>{
+  const cookieStore = await cookies();
+  cookieStore?.delete(key);
+}
