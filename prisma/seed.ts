d890265efd411b76d68e81f0b7552813263a2540
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-06 08:24:54 +0530

    * production deployment failed because of the missing type in seed.ts * added bounty and deadline field to the seed.ts file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,20 +1,27 @@
-import { PrismaClient,TicketStatus } from "@prisma/client"
+import { PrismaClient, TicketStatus } from "@prisma/client"
 const prisma = new PrismaClient();
 const tickets = [
     {
         title: "ticket 1",
         content: "this is the first ticket from db",
         status: TicketStatus.OPEN,
+        bounty: 22,
+        deadline: "2024-12-31"
     },
     {
         title: "ticket 2",
         content: " this is the second ticket from db",
-        status: TicketStatus.IN_PROGRESS
+        status: TicketStatus.IN_PROGRESS,
+        bounty: 33,
+        deadline: "2024-12-31"
+
     },
     {
         title: "ticket 3",
         content: "this is the third ticket from db",
-        status: TicketStatus.CLOSED
+        status: TicketStatus.CLOSED,
+        bounty: 33,
+        deadline: "2024-12-31"
     }
 ]
 
@@ -28,10 +35,10 @@ const seed = async () => {
             { data: tickets }
         )
     }
-    catch(err){
+    catch (err) {
         console.log(`error with prisma - ${err}`)
     }
-   
+
     const t1 = performance.now()
     console.log(`Seeding done within ${t1 - t0} seconds`)
 }
