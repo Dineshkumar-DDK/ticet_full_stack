a6ec59bbd8f7344185f85117b07cf8d8f04633dc
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-12 07:33:05 +0530

    added shadcn based alert dialogue

--- src/features/ticket/components/confirmationDialog.tsx
+++ src/features/ticket/components/confirmationDialog.tsx
@@ -1,13 +1,44 @@
 import { ActionState } from '@/components/form/utils/toActionState';
 import React from 'react'
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
 type ConfirmDialogueProps = {
+  title?: string;
+  description?: string;
   trigger: React.ReactElement;
-  action:()=>Promise<ActionState>;
+  action: () => Promise<ActionState>;
 }
-const ConfirmationDialogue = ({trigger,action}:ConfirmDialogueProps) => {
+const ConfirmationDialogue = ({
+  title = "Are you absolutely sure?",
+  description = "This action cannot be undone. This will permanently delete the item.",
+  trigger, action }: ConfirmDialogueProps) => {
   return (
-    <div>{trigger}</div>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>
+            {description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
 
