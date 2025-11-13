1d73407c9b9fb4f055f8175b4fef1683a3261b2a
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-11-13 08:15:56 +0530

    handled error for deleteTicket with utils function that uses zod

--- src/features/ticket/actions/deleteTicket.ts
+++ src/features/ticket/actions/deleteTicket.ts
@@ -5,13 +5,18 @@ import { redirect } from "next/navigation"
 import * as Path from '@/app/paths'
 import { revalidatePath } from "next/cache"
 import { deleteCookieByKey, setCookieByKey } from "@/action/cookieUtils"
-export const deleteTicket =async (id:string)=>{
-    await prisma.ticket.delete({
-        where:{
-            id,
-        }
-    })
-   
+import { formErrorToActionState } from "@/components/form/utils/toActionState"
+export const deleteTicket = async (id: string) => {
+    try {
+        await prisma.ticket.delete({
+            where: {
+                id:'abkljl',
+            }
+        })
+    } catch (err) {
+        return formErrorToActionState(err)
+    }
+
     revalidatePath(Path.ticketsPath())
     await setCookieByKey('toast', "Ticket Deleted Successfully")
     redirect(Path.ticketsPath())
