939677777dda4c0835772482c3fa1803988f4613
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-12-01 11:24:41 +0530

    implemented session logic to create and store session in the cookie

--- src/features/auth/actions/signIn.ts
+++ src/features/auth/actions/signIn.ts
@@ -1,29 +1,45 @@
 import { ticketsPath } from "@/app/paths";
-import { formErrorToActionState,ActionState, toActionState } from "@/components/form/utils/toActionState"
+import { formErrorToActionState, ActionState, toActionState } from "@/components/form/utils/toActionState"
 import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
+import { verify } from "@node-rs/argon2";
 import z from 'zod'
+import { lucia } from "@/lib/lucia";
+import { cookies } from "next/headers";
 //2
 const signInSchema = z.object({
-    email:z.email().min(1,{message:"Required"}).max(191,{message:"should be a proper email format"}),
-    password:z.string().min(6,{message:"Required"}).max(191)
+    email: z.email().min(1, { message: "Required" }).max(191, { message: "should be a proper email format" }),
+    password: z.string().min(6, { message: "Required" }).max(191)
 })
 
 // 1
-export const signIn=async (_actionState:ActionState,formData:FormData)=>{
-  try {
-    const {email,password} = signInSchema.parse(
-        Object.fromEntries(formData)
-    );
-    const user = prisma.user.findUnique({
-        where:{email}
-    })
-    if(!user){
-        return toActionState("ERROR","User not found");
-    }
-  } catch (error) {
-    return formErrorToActionState(error)
-  }
+export const signIn = async (_actionState: ActionState, formData: FormData) => {
+    try {
+        const { email, password } = signInSchema.parse(
+            Object.fromEntries(formData)
+        );
+        const user = await prisma.user.findUnique({
+            where: { email }
+        })
+        if (!user) {
+            return toActionState("ERROR", "Incorrect email or password");
+        }
+        const validPassword = await verify(user.passwordHash, password);
+        if (!validPassword) {
+            return toActionState("ERROR", "Incorrect email or password");
+        }
+        const session = await lucia.createSession(user.id, {});
+        const cookieSession = lucia.createSessionCookie(session.id);
 
-  redirect(ticketsPath())
+        const cookieStore = await cookies();
+        cookieStore?.set(
+            cookieSession.name,
+            cookieSession.value,
+            cookieSession.attributes
+        )
+    } catch (error) {
+        return formErrorToActionState(error)
+    }
+
+    redirect(ticketsPath())
 }
