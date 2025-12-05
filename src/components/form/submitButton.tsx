11188476c195ba4e6487692e8bf8c8f471b11399
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2025-12-05 10:20:52 +0530

    latest code

--- src/components/form/submitButton.tsx
+++ src/components/form/submitButton.tsx
@@ -1,31 +1,55 @@
-'use client'
-import { LucideLoaderCircle } from 'lucide-react'
-import React, { cloneElement } from 'react'
-import { useFormStatus } from 'react-dom'
-import { Button } from '@/components/ui/button'
-import clsx from 'clsx'
+"use client";
 
+import clsx from "clsx";
+import { LucideLoaderCircle } from "lucide-react";
+import { cloneElement } from "react";
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
 
 type SubmitButtonProps = {
-    label?: string,
-    icon?: React.ReactElement
+  label?: string;
+  icon?: React.ReactElement;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  size?: "default" | "sm" | "lg" | "icon";
+};
 
-}
-const SubmitButton = ({ label, icon }: SubmitButtonProps) => {
-    const { pending } = useFormStatus()
-    // const pending = true
-    return (
-        <Button disabled={pending} type='submit'>
-            {pending && (<LucideLoaderCircle className={clsx('w=4 h-4 animate-spin', { 'mr-2': !!label })} />)}
-            {label}
-            {pending ? null : icon ? (
-                <span className={clsx({ "ml-2": !!label })}>
-                    {cloneElement(icon, { className: 'h-4 w-4' })}
-                </span>
-            ) : null}
+const SubmitButton = ({
+  label,
+  icon,
+  variant = "default",
+  size = "default",
+}: SubmitButtonProps) => {
+  const { pending } = useFormStatus();
 
-        </Button>
-    )
-}
+  return (
+    <Button disabled={pending} type="submit" variant={variant} size={size}>
+      {pending && (
+        <LucideLoaderCircle
+          className={clsx("h-4 w-4 animate-spin", {
+            "mr-2": !!label,
+          })}
+        />
+      )}
+      {label}
+      {pending ? null : icon ? (
+        <span
+          className={clsx({
+            "ml-2": !!label,
+          })}
+        >
+          {cloneElement(icon, {
+            className: "w-4 h-4",
+          })}
+        </span>
+      ) : null}
+    </Button>
+  );
+};
 
-export default SubmitButton
+export default SubmitButton ;
